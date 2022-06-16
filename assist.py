14409a0afa302398842fc3fca6be53d3efdb0f1e
shankar <shankarkharel35@gmail.com>
2022-06-16 20:11:42 +0545

    initial commit

--- assist.py
+++ assist.py
@@ -1,38 +1,43 @@
 import pyttsx3
 import datetime
-import speech_recognition as sr
+# import speech_recognition as sr
 import wikipedia
 import webbrowser
-import os
-import smtplib
-import pythoncom
+# import os
+# import smtplib
+# import pythoncom
 import requests
+
 res = requests.get('https://ipinfo.io/')
 data = res.json()
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[1].id)
-#print(voices[1].id)
-
-#print(voices)
+engine.setProperty('voice', voices[1].id)
 
 
+# print(voices[1].id)
+
+# print(voices)
 
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+
 def wishme():
     hrs = int(datetime.datetime.now().hour)
 
-    if hrs>=12 and hrs<0:
-            speak("good morning!")
-    elif hrs>=0 and hrs<5:
-         speak("good afternoon!")
-    elif hrs>5 and time<8:
+    if hrs >= 12 and hrs < 0:
+        speak("good morning!")
+    elif hrs >= 0 and hrs < 5:
+        speak("good afternoon!")
+    elif hrs > 5 and time < 8:
         speak('good evening!')
     else:
-            speak("hello there!")
+        speak("hello there!")
+
+
 def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -43,7 +48,7 @@ def takecommand():
     query = None
     try:
         print("recogniting...")
-        query = r.recognize_google(audio,language='en-uk')
+        query = r.recognize_google(audio, language='en-uk')
 
         print(f"user said : {query}\n")
     except Exception as e:
@@ -51,23 +56,23 @@ def takecommand():
         query = None
 
     return query
-#program starts.
+
+
+# program starts.
 speak("i am assistant to this device.")
 speak('how can i help you?')
 wishme()
 
-query=takecommand()
+query = takecommand()
 
-
-
-#logic for executing task.
-if 'exit'in query.lower():
-        say('thank you!')
+# logic for executing task.
+if 'exit' in query.lower():
+    say('thank you!')
 
 elif 'Wikipedia' in query:
     speak("searching wikipedia....")
-    query = query.replace("wikipedia","")
-    results = wikipedia.summary(query,sentences=3)
+    query = query.replace("wikipedia", "")
+    results = wikipedia.summary(query, sentences=3)
     speak(results)
 
 elif 'open youtube' in query.lower():
@@ -82,4 +87,3 @@ elif 'Google' in query:
 elif 'location' in query.lower():
     speak('opening google maps')
     webbrowser.open('www.google.com/maps')
-
